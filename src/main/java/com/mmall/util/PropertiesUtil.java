6634e2de42dff8b47bfea070c2a8b441bf4d163b
zhangliangbj <zhangliang1726@163.com>
2018-12-21 15:35:14 +0800

    fix commit

--- src/main/java/com/mmall/util/PropertiesUtil.java
+++ src/main/java/com/mmall/util/PropertiesUtil.java
@@ -21,6 +21,11 @@ public class PropertiesUtil {
         String fileName = "mmall.properties";
         props = new Properties();
         try {
+            //inputStreamReader字节流通向字符流的桥梁
+            //load():按简单的面向行的格式从输入字符流中读取属性列表（键和元素对）。
+            //getClass()：取得当前对象所属的Class对象;PropertiesUtil.class也是得到对象
+            //getClassLoader()：取得该Class对象的类装载器
+            //理解class.getClassLoader().getResourceAsStream(file)和class.getResourceAsStream(file)，都是实现获取在classpath路径下的资源文件的输入流。
             props.load(new InputStreamReader(PropertiesUtil.class.getClassLoader().getResourceAsStream(fileName),"UTF-8"));
         } catch (IOException e) {
             logger.error("配置文件读取异常",e);
